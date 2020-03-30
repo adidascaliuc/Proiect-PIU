67db15e450d0e0715047f41e4f133601012e2c13
adidascaliuc <62601146+adidascaliuc@users.noreply.github.com>
2020-03-30 17:11:03 +0300

    Enum

---
+++ Targ De Masini/Enumerari.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Targ_De_Masini
+{
+    public enum Culoare
+    {
+        Alb = 1,  // stiu ca sunt nonculori :))
+        Negru = 2,  // stiu ca sunt nonculori :))
+        Rosu = 3,
+        Albastru = 4,
+        Verde = 5,
+    }
+}
